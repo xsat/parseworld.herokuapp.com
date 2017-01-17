cf4832969fa3baefbbe33222ef4679a7cbca70d6
Yuri Antonchyk <yurii.antonchyk@gmail.com>
2017-01-17 22:16:46 +0200

    New styles

--- public/index.php
+++ public/index.php
@@ -1,12 +1,23 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Did i parse all world?</title>
+    <title>Did i parse all world? - ParseWorld</title>
     <meta content="text/html; charset=utf-8" http-equiv="Content-type">
     <meta content="width=device-width, initial-scale=1, user-scalable=0" name="viewport">
     <meta charset="UTF-8">
+    <meta property="og:title" content="Did i parse all world? - ParseWorld" />
+    <meta property="og:type" content="website"> /
+    <meta property="og:url" content="https://parseworld.herokuapp.com/" />
+    <meta property="og:image" content="https://parseworld.herokuapp.com/img/logo.png" />
+    <link rel="shortcut icon" type="image/x-icon" href="/img/icon.png" />
+    <link rel="stylesheet" type="text/css" href="/css/styles.css" />
 </head>
 <body>
-    <h1>Let's parse all global web</h1>
-    <h2>Simply - perfect</h2>
+    <div class="container">
+        <a class="logo" href="/">
+            <img title="ParseWorld" alt="ParseWorld" src="/img/logo.png">
+        </a>
+        <h1>Let's parse all global web</h1>
+        <h2>Simply - perfect</h2>
+    </div>
 </body>
