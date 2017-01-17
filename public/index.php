f3e39045d43bb818688bfb8861bc824424933038
Yuri Antonchyk <yurii.antonchyk@gmail.com>
2017-01-17 23:21:31 +0200

    Close tags

--- public/index.php
+++ public/index.php
@@ -2,19 +2,19 @@
 <html>
 <head>
     <title>Did i parse all world? - ParseWorld</title>
-    <meta content="text/html; charset=utf-8" http-equiv="Content-type">
-    <meta content="width=device-width, initial-scale=1, user-scalable=0" name="viewport">
-    <meta charset="UTF-8">
+    <meta content="text/html; charset=utf-8" http-equiv="Content-type" />
+    <meta content="width=device-width, initial-scale=1, user-scalable=0" name="viewport" />
+    <meta charset="UTF-8" />
     <meta property="og:title" content="Did i parse all world? - ParseWorld" />
-    <meta property="og:type" content="website"> /
+    <meta property="og:type" content="website" />
     <meta property="og:url" content="https://parseworld.herokuapp.com/" />
-    <meta property="og:image" content="https://parseworld.herokuapp.com/img/logo.png" />
-    <link rel="shortcut icon" type="image/x-icon" href="https://parseworld.herokuapp.com/img/favicon.ico" />
-    <link rel="icon" href="https://parseworld.herokuapp.com/img/favicon_32.png" sizes="32x32" />
-    <link rel="icon" href="https://parseworld.herokuapp.com/img/favicon_48.png" sizes="48x48" />
-    <link rel="icon" href="https://parseworld.herokuapp.com/img/favicon_96.png" sizes="96x96" />
-    <link rel="icon" href="https://parseworld.herokuapp.com/img/favicon_144.png" sizes="144x144" />
-    <link rel="stylesheet" type="text/css" href="/css/styles.css" />
+    <meta property="og:image" content="//parseworld.herokuapp.com/img/logo.png" />
+    <link rel="shortcut icon" type="image/x-icon" href="//parseworld.herokuapp.com/img/favicon.ico" />
+    <link rel="icon" href="//parseworld.herokuapp.com/img/favicon_32.png" sizes="32x32" />
+    <link rel="icon" href="//parseworld.herokuapp.com/img/favicon_48.png" sizes="48x48" />
+    <link rel="icon" href="//parseworld.herokuapp.com/img/favicon_96.png" sizes="96x96" />
+    <link rel="icon" href="//parseworld.herokuapp.com/img/favicon_144.png" sizes="144x144" />
+    <link rel="stylesheet" href="/css/styles.css" type="text/css" />
 </head>
 <body>
     <div class="container">
