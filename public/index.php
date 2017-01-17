a5731304dd72f068aff668426fe6a81577167186
Yuri Antonchyk <yurii.antonchyk@gmail.com>
2017-01-17 23:24:05 +0200

    Index hotfix

--- public/index.php
+++ public/index.php
@@ -14,12 +14,12 @@
     <link rel="icon" href="//parseworld.herokuapp.com/img/favicon_48.png" sizes="48x48" />
     <link rel="icon" href="//parseworld.herokuapp.com/img/favicon_96.png" sizes="96x96" />
     <link rel="icon" href="//parseworld.herokuapp.com/img/favicon_144.png" sizes="144x144" />
-    <link rel="stylesheet" href="/css/styles.css" type="text/css" />
+    <link rel="stylesheet" href="//parseworld.herokuapp.com/css/styles.css" type="text/css" />
 </head>
 <body>
     <div class="container">
         <a class="logo" href="/">
-            <img title="ParseWorld" alt="ParseWorld" src="/img/logo.png">
+            <img title="ParseWorld" alt="ParseWorld" src="//parseworld.herokuapp.com/img/logo.png">
         </a>
         <h1>Let's parse all global web</h1>
         <h2>Simply - perfect</h2>
